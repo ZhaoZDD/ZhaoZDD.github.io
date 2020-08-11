7e4de6cb62dec2e6d62de53709cda56e2b8331af
aboydreamzzd <aboydreamzzd@163.com>
2020-08-11 14:39:37 +0800

    Site updated: 2020-08-11 14:39:31

--- js/ui/mode.js
+++ js/ui/mode.js
@@ -7,19 +7,24 @@ const rootElementDarkModeAttributeName = "data-user-color-scheme";
 const setLS = (k, v) => {
   try {
     localStorage.setItem(k, v);
-  } catch (e) {}
+  } catch (e) {
+    console.log(e.message);
+  }
 };
 
 const removeLS = (k) => {
   try {
     localStorage.removeItem(k);
-  } catch (e) {}
+  } catch (e) {
+    console.log(e.message);
+  }
 };
 
 const getLS = (k) => {
   try {
     return localStorage.getItem(k);
   } catch (e) {
+    console.log(e.message);
     return null;
   }
 };
@@ -74,8 +79,11 @@ const toggleCustomDarkMode = () => {
 
 applyCustomDarkModeSettings();
 
-document.addEventListener("DOMContentLoaded", () => {
+function bindToggleButton() {
   document.getElementById("toggle-mode-btn").addEventListener("click", () => {
     applyCustomDarkModeSettings(toggleCustomDarkMode());
   });
-});
+}
+
+document.addEventListener("DOMContentLoaded", bindToggleButton);
+document.addEventListener("pjax:success", bindToggleButton);
